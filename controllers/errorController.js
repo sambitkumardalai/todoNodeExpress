82af71b0bdd218c36e49dd088d75306a3dff5845
sambit <sambitskd3@gmail.com>
2021-11-14 09:31:41 +0530

    CastError handled in production mode

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,42 @@
+const AppError = require("../utils/appError");
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  console.log(err.isOperational);
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log(`Error 💥`, err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong",
+    });
+  }
+};
 //step-3: global error handling middleware
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = Object.create(err);
+    if (err.name === "CastError") error = handleCastError(err);
+    sendErrorProd(error, res);
+  }
 };
